d0823a9da64b208adbbe95b6963fb7c31c1c1810
Stéphane Noutsa <stephanenoutsak@gmail.com>
2023-11-24 00:39:34 +0100

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "AWS_ACCESS_KEY_ID" {
+  type = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type = string
+}
+
+variable "AWS_SESSION_TOKEN" {
+  type    = string
+  default = null
+}
+
+variable "AWS_REGION" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
+
+variable "tags" {
+  type = map(string)
+}
